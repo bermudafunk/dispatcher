ebc7066b9affae4380e0c864f8edd22c51d1aa7f
Christian Kohlstedde <christian@kohlsted.de>
2024-06-12 23:37:09 +0200

    Disable debug log level of symnet_cp

--- bermudafunk/base/__init__.py
+++ bermudafunk/base/__init__.py
@@ -4,12 +4,16 @@ import typing
 
 import config
 
-logging.basicConfig(format="%(asctime)s : %(levelname)8s : %(name)30s : %(funcName)-20s : %(lineno)4d : %(message)s",level=logging.DEBUG)
+logging.basicConfig(
+    format="%(asctime)s : %(levelname)8s : %(name)30s : %(funcName)-20s : %(lineno)4d : %(message)s",
+    level=logging.INFO,
+)
+
 logging.getLogger("transitions").setLevel(logging.INFO)
 
 if config.DEBUG:
     logging.getLogger("bermudafunk").setLevel(logging.DEBUG)
-    logging.getLogger("symnet_cp").setLevel(logging.DEBUG)
+    # logging.getLogger("symnet_cp").setLevel(logging.DEBUG)
     logging.getLogger("transitions").setLevel(logging.DEBUG)
 
 logger = logging.getLogger(__name__)
